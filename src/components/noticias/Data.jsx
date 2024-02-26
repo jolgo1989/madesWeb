3ec330c955dc3fd84f9103de28029741b53b4e42
jolgo <jlgo60@hotmail.com>
2024-02-26 05:11:48 -0500

    se modifico section noticias

---
+++ src/components/noticias/Data.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+export const images = [
+  {
+    url: "https://www.clinicaveterinariazarpa.com/wp-content/uploads/2020/11/consejos-cuidar-iguana.jpg",
+    title: "titulo1",
+    Typography:
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  },
+  {
+    url: "https://static.miscota.com/consejos/wp-content/uploads/2016/12/shutterstock_433466308-720x387.jpg",
+    title: "titulo2",
+    Typography:
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  },
+  {
+    url: "https://media.istockphoto.com/id/622013488/es/foto/drag%C3%B3n-de-mar-chino.webp?b=1&s=170667a&w=0&k=20&c=F8WWe-KY1wGRHEUuIhobTWuw7MTRlCJBn8XZxnt4Ddo=",
+    title: "titulo3",
+    Typography:
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  },
+  {
+    url: "https://www.clinicaveterinariazarpa.com/wp-content/uploads/2020/11/consejos-cuidar-iguana.jpg",
+    title: "titulo3",
+    Typography:
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  },
+];
--- src/components/noticias/Noticias.jsx
+++ src/components/noticias/Noticias.jsx
@@ -1,64 +1,104 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import { FaDisplay } from "react-icons/fa6";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
-const images = [
-  {
-    url: "https://www.clinicaveterinariazarpa.com/wp-content/uploads/2020/11/consejos-cuidar-iguana.jpg",
-    title: "titulo1",
-    Typography:
-      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-  },
-  {
-    url: "https://static.miscota.com/consejos/wp-content/uploads/2016/12/shutterstock_433466308-720x387.jpg",
-    title: "titulo2",
-    Typography:
-      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-  },
-  {
-    url: "https://media.istockphoto.com/id/622013488/es/foto/drag%C3%B3n-de-mar-chino.webp?b=1&s=170667a&w=0&k=20&c=F8WWe-KY1wGRHEUuIhobTWuw7MTRlCJBn8XZxnt4Ddo=",
-    title: "titulo3",
-    Typography:
-      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-  },
-];
+import { images } from "./Data";
+
+const NoticiaCard = ({ image }) => {
+  return (
+    <Card sx={{ width: "25%" }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image.url}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {image.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {image.Typography}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 const Noticias = () => {
   return (
-    <Card
+    <Box
       sx={{
-        background: "#303030",
         display: "flex",
-        maxWidth: "90%",
+        flexDirection: "row",
+        gap: 1,
+        maxWidth: 1366,
+        margin: "auto",
       }}
     >
-      {images.map((image) => (
-        <CardActionArea
-          sx={{
-            padding: 2,
-          }}
-        >
+      <Card sx={{ width: "70%" }}>
+        <CardActionArea sx={{ display: "flex", flexDirection: "row" }}>
           <CardMedia
             component="img"
             height="140"
-            image={image.url}
+            image="https://www.clinicaveterinariazarpa.com/wp-content/uploads/2020/11/consejos-cuidar-iguana.jpg"
+            alt="green iguana"
+            sx={{ width: "88%" }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+
+        <Box sx={{ display: "flex", gap: 1, marginTop: 2 }}>
+          {images.map((image, index) => (
+            <NoticiaCard key={index} image={image} />
+          ))}
+        </Box>
+      </Card>
+
+      <Card sx={{ width: "30%" }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://www.clinicaveterinariazarpa.com/wp-content/uploads/2020/11/consejos-cuidar-iguana.jpg"
             alt="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              {image.title}
+              Lizard
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              {image.Typography}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+              molestiae! Laboriosam excepturi fugiat ex cupiditate et eius quos
+              magni reprehenderit nobis aspernatur eveniet, facere sunt unde
+              iure doloremque corrupti repellat. Lizards are a widespread group
+              of squamate reptiles, with over 6,000 species, ranging across all
+              continents except Antarctica Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Pariatur, molestiae! Laboriosam
+              excepturi fugiat ex cupiditate et eius quos magni reprehenderit
+              nobis aspernatur eveniet, facere sunt unde iure doloremque
+              corrupti repellat.
             </Typography>
           </CardContent>
         </CardActionArea>
-      ))}
-    </Card>
+      </Card>
+    </Box>
   );
 };
 
