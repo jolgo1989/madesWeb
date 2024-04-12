21ac2c6bebe24fbb952f6caf7b3570356f31403e
jolgo <jlgo60@hotmail.com>
2024-04-11 21:03:10 -0500

    Se agregaron motones a la seccion quienes somos

--- src/components/quienesSomos/QuienesSomos.jsx
+++ src/components/quienesSomos/QuienesSomos.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from "react";
-import "./QuienesSomos.scss";
+import React from "react";
 import ImgComponent from "./ImgComponent";
 import { Transition } from "../../Variants";
-import Contacts from "../contacto/contacts";
+import { Stack, Button } from "@mui/material";
+import { FaCheck } from "react-icons/fa"; // Importa el icono de chulo de React Icons
+import "./QuienesSomos.scss";
 
 const QuienesSomos = () => {
   return (
@@ -12,22 +13,16 @@ const QuienesSomos = () => {
         transitionTime={1}
         effectDuration={0.5}
       >
-        <div className="principalTitle">
+        <div className="title">
           <h1>
-            <b>SOBRE </b> NOSOTROS
-          </h1>
-        </div>
-        <div className="secundaryTitle">
-          <h1>
-            <b>HACIENDO HISTORIA </b> DESDE 2014
+            <b>Sirviendo </b> desde 2014
           </h1>
         </div>
       </Transition>
-      <div
-        className="listAbout"
-        // titleClassName={"listAbout"}
-        // transitionTime={1.5}
-        // effectDuration={1}
+      <Transition
+        titleClassName={"listAbout"}
+        transitionTime={1.5}
+        effectDuration={1}
       >
         <div className="textAbout">
           <h1>Historia</h1>
@@ -41,19 +36,31 @@ const QuienesSomos = () => {
             Est nonumy sed labore eirmod sit magna. Erat at est justo sit ut.
             Labor diam sed ipsum et eirmod
           </p>
+          <Stack spacing={2} direction="row">
+            <Button variant="contained">Read more</Button>
+          </Stack>
         </div>
         {/*Licencia <a href="https://www.freepik.com">designed by jannoon028 - Freepik.com</a> */}
         <img src="./public/2096.png" alt="teacher" />
         <div className="textAbout">
           <h1>Visión</h1>
           <p>
-            Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo
-            dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat
-            justo sed sed diam. Ea et erat ut sed diam sea ipsum est dolor
+            Takimata sed vero vero no sit sed, justo clita duo no duo amet et,
+            nonumy kasd sed dolor eos diam lorem eirmod. Amet sit amet amet no.
+            Est nonumy sed labore eirmod sit magna. Erat at est justo sit ut.
+            Labor diam sed ipsum et eirmod
           </p>
-          <p></p>
+          <div className="textItem">
+            <li className="item">Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+          </div>
+
+          <Stack spacing={2} direction="row">
+            <Button variant="contained">Read more</Button>
+          </Stack>
         </div>
-      </div>
+      </Transition>
     </div>
   );
 };
