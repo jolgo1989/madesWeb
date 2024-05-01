64092d185ebb62b3d216ec25484f28d8dd7662b9
jolgo <jlgo60@hotmail.com>
2024-05-01 08:01:00 -0500

    Se eliminaron card de noticias

--- src/components/noticias/CardNoticias.jsx
+++ src/components/noticias/CardNoticias.jsx
@@ -1,12 +1,4 @@
 import * as React from "react";
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-} from "@mui/material";
 import { images } from "./NewsData";
 
 import "./CardNoticias.scss";
@@ -14,45 +6,38 @@ import "./CardNoticias.scss";
 const ImgCard = ({ image, size }) => {
   const className = `card-size-${size}`;
   return (
-    <Card className={className}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          // height="140"
-          image={image.url}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {image.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {image.Typography}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className={className}>
+      <img src={image.url} alt="green-iguana" />
+      <div>
+        {image.title}
+        {image.Typography}
+      </div>
+    </div>
   );
 };
 
 const CardNoticias = () => {
   return (
-    <Box className="container">
+    <div className="container">
       {images.map((image, index) => {
         // lógica para asignar tamaños
         // Por ejemplo, asignar tamaños basados en el índice:
         let size;
-        if (index % 3 === 0) {
+        if (index % 5 === 0) {
           size = 1; // Tamaño pequeño
-        } else if (index % 3 === 1) {
+        } else if (index % 5 === 1) {
           size = 2; // Tamaño mediano
-        } else if (index % 3 === 2) {
+        } else if (index % 5 === 2) {
           size = 3; // Tamaño mediano
+        } else if (index % 5 === 3) {
+          size = 4;
+        } else {
+          size = 5;
         }
 
         return <ImgCard key={index} image={image} size={size} />;
       })}
-    </Box>
+    </div>
   );
 };
 
--- src/components/noticias/NewsData.jsx
+++ src/components/noticias/NewsData.jsx
@@ -3,7 +3,7 @@ export const images = [
     url: "https://images.pexels.com/photos/4613878/pexels-photo-4613878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "titulo1",
     Typography:
-      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
   },
   {
     url: "https://images.pexels.com/photos/3207532/pexels-photo-3207532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
@@ -17,17 +17,17 @@ export const images = [
     Typography:
       " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
   },
-  // {
-  //   url: "https://images.pexels.com/photos/4314674/pexels-photo-4314674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //   title: "titulo4",
-  //   Typography:
-  //     " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-  // },
+  {
+    url: "https://images.pexels.com/photos/4314674/pexels-photo-4314674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "titulo4",
+    Typography:
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  },
 
-  // {
-  //   url: "https://images.pexels.com/photos/1670045/pexels-photo-1670045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  //   title: "titulo5",
-  //   Typography:
-  //     " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-  // },
+  {
+    url: "https://images.pexels.com/photos/1670045/pexels-photo-1670045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "titulo5",
+    Typography:
+      " Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+  },
 ];
